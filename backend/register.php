e46d0db6d239d2bf36a26fe1dd6d0ec04b79d0ca
asc3rr <borysgnacinski@gmail.com>
2021-01-29 15:21:36 +0100

    better validating

--- backend/register.php
+++ backend/register.php
@@ -1,6 +1,11 @@
 <?php
     require_once("db.php");
 
+    if(!isset($_POST['nick']) && empty($_POST['nick']) && !isset($_POST['pass1']) && empty($_POST['pass1']) && !isset($_POST['pass2']) && empty($_POST['pass2'])){
+        header("Location: ../");
+        die;
+    }
+
     $nick = $_POST['nick'];
     $pass1 = $_POST['pass1'];
     $pass2 = $_POST['pass2'];
